5efb3ba053e51306fc5b25ddc6f9c094a91a8576
sebaek <sebaek@gmail.com>
2024-05-09 16:16:38 +0900

    배열map 메소드로 배열의 각 원소를 컴포넌트로 매핑할 수 있음 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  const foodList = ["pizza", "burger", "noodle", "coffee"];
+
+  return (
+    <div>
+      {/*
+    <ul>
+      <li> pizza </li>
+      <li> burger </li>
+      <li> noodle </li>
+      <li> coffee </li>
+    </ul>
+  */}
+    </div>
+  );
 }
 
 export default App;
