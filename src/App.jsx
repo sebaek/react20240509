2827c8859b66c5542d8262b1637e921e9f6c7741
sebaek <sebaek@gmail.com>
2024-05-09 12:04:17 +0900

    jsx에서 자바스크립트 값 사용하기 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div></div>;
+  const name = "강인";
+  const position = "미드필더";
+  const salary = 30;
+
+  return <div>{/* <p> 미드필더 강인의 연봉은 30억입니다. </p> */}</div>;
 }
 
 export default App;
