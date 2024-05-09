607ac694e400e6cafb7d1568327733fde9e4367c
sebaek <sebaek@gmail.com>
2024-05-09 14:11:23 +0900

    &&(and ...이면), ||(or ...아니면) 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  return <div></div>;
+  const name = "강인";
+  const age = 5; // or 30;
+
+  return (
+    <div>
+      <p>{name}</p>
+      {name === "흥민" && <p>공격수</p>}
+      {name === "강인" && <p>미드필더</p>}
+
+      {/*
+    age 가 20 보다 크거나 같으면
+    <p> 투표 가능 </p>
+    age 가 20 보다 작으면
+    <p> 투표 불가능 </p>
+     */}
+    </div>
+  );
 }
 
 export default App;
