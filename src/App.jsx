38423c1c8c46157f5be82b81c6ebb0c7e77b70e0
sebaek <sebaek@gmail.com>
2024-05-09 16:33:51 +0900

    javascript string, "", '', ``

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // string
+  const s1 = "some string";
+  const s2 = "other string";
+  const s3 = "I'm 신뢰에요";
+  const s4 = '제 이름은 "흥민"입니다.';
+
+  // backtick
+  // template literal
+  const s5 = `another string`;
+
+  const name = "son";
+  const age = 30;
+
+  const s6 = name + " is " + age * 2 + " years.";
+  const s7 = `${name} is ${age * 2} years.`;
+
+  return (
+    <div>
+      <p>{s6}</p>
+      <p>{s7}</p>
+    </div>
+  );
 }
 
 export default App;
