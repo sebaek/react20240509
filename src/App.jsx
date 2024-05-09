314d0b649aa00a8c39e724f01b90731c7bd50909
sebaek <sebaek@gmail.com>
2024-05-09 11:25:50 +0900

    컴포넌트 만들기 연습 시작

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+// todo; MyHeader, MyFooter, MyNav 컴포넌트 만들기
+
 function App() {
-  return <div>hello react</div>;
+  return (
+    <div>
+      <MyHeader />
+      {/*
+    <div>
+      <h1> hello header </h1>
+      <h3> hello sub header </h3>
+    </div>
+    */}
+      <MyFooter />
+      {/*
+      <ul>
+        <li>link1</li>
+        <li>link2</li>
+      </ul>
+    */}
+      <MyNav />
+      {/*
+       <a href="#">link 1</a>
+       <a href="#">link 2</a>
+       */}
+    </div>
+  );
 }
 
 export default App;
