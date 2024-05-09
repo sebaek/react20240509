521525bbacb9f0b563122127cc02da64e3893402
sebaek <sebaek@gmail.com>
2024-05-09 15:16:16 +0900

    javascript object와 property 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,17 @@ function App(props) {
     email: "john@example.com",
     password: "1234567890",
   };
-  return <div>{/* obj의 각 프로퍼티를 jsx 내에서 출력하는 코드 작성 */}</div>;
+  return (
+    <div>
+      {/* obj의 각 프로퍼티를 jsx 내에서 출력하는 코드 작성 */}
+      <ul>
+        <li>age : {obj.age}</li>
+        <li>name : {obj.name}</li>
+        <li>email : {obj.email}</li>
+        <li>pw : {obj.password}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
