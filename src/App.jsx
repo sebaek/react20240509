ba8b77ae3ce11a547d56527d0b0a4a1fe1af45e1
sebaek <sebaek@gmail.com>
2024-05-09 12:25:39 +0900

    jsx 내에서 {}로 자바스크립트 코드 작성 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ function App() {
   const a = 5;
   const b = 9;
 
+  function someMethod() {
+    return "hello js";
+  }
+
+  function someMethod2(param) {
+    return param + " " + param;
+  }
+
   return (
     <>
       <div>
         {/* <p> 5 X 9 = 45 </p> */}
+        <p>
+          {a} X {b} = {a * b}
+        </p>
         <p>{someMethod()}</p> {/* hello js */}
         <p>{someMethod2("react")}</p> {/* react react */}
       </div>
