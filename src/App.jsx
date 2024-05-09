51b16a23f4fcafbba2ad65411130ba74fb47a1b1
sebaek <sebaek@gmail.com>
2024-05-09 11:04:47 +0900

    컴포넌트 만들기 연습 시작

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+// todo : 이 위치에 MyTag, MyContent 컴포넌트 만들기
+
 function App() {
   return (
     <>
       <div>
         <h1>hello react</h1>
+        <MyTag />
+        {/* <h3> hello component </h3>*/}
+        <MyContent />
+        {/* <p> my content </p> */}
       </div>
     </>
   );
