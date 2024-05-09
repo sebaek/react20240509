7d17b1a409f21132b13b54951c6be3b392140d76
sebaek <sebaek@gmail.com>
2024-05-09 14:47:42 +0900

    javascript object

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 function App(props) {
+  // object : {}
+  //  {} 안에 property명 : property값 쌍들이 작성됨
+  let a = {};
+  let b = { name: "흥민" };
+  let c = { address: "신촌", city: "서울", country: "한국" };
+
+  console.log("a", a);
+  console.log("b", b);
+  console.log("c", c);
+
   return <div></div>;
 }
 
