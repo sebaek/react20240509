13fbcd7ea1de8b0398910d179982fadedcff7f79
sebaek <sebaek@gmail.com>
2024-05-09 11:37:29 +0900

    컴포넌트 만들기 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 // todo; MyHeader, MyFooter, MyNav 컴포넌트 만들기
+function MyHeader() {
+  return (
+    <div>
+      <h1>hello header</h1>
+      <h3>hello sub header</h3>
+    </div>
+  );
+}
 
+function MyFooter() {
+  return (
+    <ul>
+      <li>link 1</li>
+      <li>link 2</li>
+    </ul>
+  );
+}
+
+// 다른 컴포넌트 안에 컴포넌트를 작성하면 안됨
+function MyNav() {
+  return (
+    <>
+      <a href="#">link1</a>
+      <a href="#">link2</a>
+    </>
+  );
+}
 function App() {
   return (
     <div>
