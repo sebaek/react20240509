aa31b5181ae581872299b6990447847b67972f8f
sebaek <sebaek@gmail.com>
2024-05-10 15:43:56 +0900

    이벤트 props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <button
+        onClick={() => console.log("clicked")}
+        onMouseEnter={() => console.log("mouse entered")}
+        onMouseLeave={() => console.log("mouse leaved")}
+      >
+        클릭1
+      </button>
+      <input type="text" onChange={() => console.log("changed")} />
+    </div>
+  );
 }
 
 export default App;
