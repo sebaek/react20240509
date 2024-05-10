fb2fcd1ebf0ae57a1901eeb8b6fd467ffb93a55e
sebaek <sebaek@gmail.com>
2024-05-10 16:08:35 +0900

    style props은 객체로
    css property 명은 lowerCamelCase 로

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // style props
+
+  return (
+    <div>
+      <div
+        style={{
+          color: "blue",
+          padding: "10px",
+          border: "2px dotted green",
+          textAlign: "center",
+          backgroundColor: "aqua",
+        }}
+      >
+        Lorem ipsum dolor.
+      </div>
+    </div>
+  );
 }
 
 export default App;
