c3c42df065a0cbc24a7cc3ec83ce1d0f2a0e758c
sebaek <sebaek@gmail.com>
2024-05-10 15:27:17 +0900

    props 활용 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton name="샾 출력" onClick={() => console.log("####")} />
+      <MyButton name="느낌표 출력" onClick={() => console.log("!!!!!")} />
+    </div>
+  );
 }
 
 export default App;
