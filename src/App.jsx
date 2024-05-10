58bf09273c27d25fe470e6dfec3b9a2d49c81011
sebaek <sebaek@gmail.com>
2024-05-10 14:28:59 +0900

    props를 분해할당으로 받기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
-function App(props) {
+function MyElem(props) {
+  console.log(props); // {name: "son", age: 33}
+
+  const { name, age } = props;
+  console.log("name", name); // son
+  console.log("age", age); // 33
+
   return <div></div>;
+}
+
+function MyComp({ city, country, price }) {
+  // props : {city: "seoul", country:"korea", price: 500}
+  console.log("city", city);
+  console.log("country", country);
+  console.log("price", price);
+
+  return <div></div>;
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyElem name="son" age={33} />
+      <MyComp city="seoul" country="korea" price={500} />
+    </div>
+  );
 }
 
 export default App;
