c362ca65f55ad0b7c89a486dde69ed328eff198f
sebaek <sebaek@gmail.com>
2024-05-10 11:47:25 +0900

    얕은 복사, 깊은 복사

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 
 function App(props) {
+  const a = {
+    name: "John",
+    age: 44,
+    address: {
+      street: "Lorem Ipsum",
+      city: "seoul",
+    },
+  };
+
+  // 얕은 복사 (shallow copy)
+  const { ...b } = a;
+  b.age = 55;
+  console.log("b.age", b.age); // 55
+  console.log("a.age", a.age); // 44
+
+  b.address.city = "busan";
+  console.log("a.address.city", a.address.city); // busan
+  console.log("b.address.city", b.address.city); // busan
+
+  // 깊은 복사 (deep copy)
+
   return <div></div>;
 }
 
