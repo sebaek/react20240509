dce15fc63e23a594917eb0c73721b2289156196a
sebaek <sebaek@gmail.com>
2024-05-10 16:09:08 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // style props
-
-  return (
-    <div>
-      <div
-        style={{
-          color: "blue",
-          padding: "10px",
-          border: "2px dotted green",
-          textAlign: "center",
-          backgroundColor: "aqua",
-        }}
-      >
-        Lorem ipsum dolor.
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
