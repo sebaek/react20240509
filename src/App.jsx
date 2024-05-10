ea952b3294153f431c238c28392557de162ac554
sebaek <sebaek@gmail.com>
2024-05-10 11:14:03 +0900

    배열의 값들을 분해할당, 없으면 기본값 할당

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,32 @@ function App(props) {
   const b = ["pizza", 3.14, "lunch"];
 
   // 적절한 코드 작성
+  const [x1, x2, x3] = b;
 
   console.log("x1", x1); // pizza
   console.log("x2", x2); // 3.14
   console.log("x3", x3); // lunch
+
+  const c = ["son", "lee"];
+  const [a1] = c;
+  console.log("a1", a1); // son
+
+  const [b1, b2, b3] = c;
+  console.log("b3", b3); // undefined
+
+  const [c1, c2 = "park", c3 = "kim"] = c;
+  console.log("c1", c1); // son
+  console.log("c2", c2); // lee
+  console.log("c3", c3); // kim
+
+  // 연습
+  const d = ["name", "email", "phone"];
+  // 분해할당 코드 작성
+
+  console.log("d1", d1); // name
+  console.log("d2", d2); // email
+  console.log("d3", d3); // phone
+  console.log("d4", d4); // city
 
   return <div></div>;
 }
