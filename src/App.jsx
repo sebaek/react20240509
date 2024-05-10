eeff80de78722f78b39ceee688810b3fe941f1b4
sebaek <sebaek@gmail.com>
2024-05-10 15:23:01 +0900

    props에 값이 없이 이름만 작성되면 값이 true

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-function App(props) {
+function MyComp({ name, age, someProp, otherProp }) {
+  console.log("name", name);
+  console.log("age", age);
+  console.log("someProp", someProp); // true :값없이 작성되면 true
+  console.log("otherProp", otherProp); // true
   return <div></div>;
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyComp name="son" age={30} someProp otherProp={true} />
+    </div>
+  );
 }
 
 export default App;
