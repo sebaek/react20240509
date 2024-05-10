9edf67e04c550a02c3da4693b0e1f784b8c33e4b
sebaek <sebaek@gmail.com>
2024-05-10 16:31:29 +0900

    style props 응용 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,22 @@ function App(props) {
   return (
     <div>
       {/*  두 개의 MyButton 컴포넌트를 사용 sx, name는 여러분 취향껏 작성 */}
+      <MyButton
+        name={"red button"}
+        sx={{
+          backgroundColor: "red",
+          color: "white",
+          padding: "10px",
+        }}
+      ></MyButton>
+      <MyButton
+        name={"blue button"}
+        sx={{
+          backgroundColor: "blue",
+          color: "white",
+          padding: "10px",
+        }}
+      ></MyButton>
     </div>
   );
 }
