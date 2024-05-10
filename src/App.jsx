ddb980ec3256f8ebbcd7acb372dc5a35bbaa0ba8
sebaek <sebaek@gmail.com>
2024-05-10 16:25:13 +0900

    style props 응용 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+function MyButton({ name, sx }) {
+  return <button style={sx}>{name}</button>;
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      {/*  두 개의 MyButton 컴포넌트를 사용 sx, name는 여러분 취향껏 작성 */}
+    </div>
+  );
 }
 
 export default App;
