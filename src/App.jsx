c7277fa4fb8aaa563c6bc868afd639975ebe1aff
sebaek <sebaek@gmail.com>
2024-05-13 16:34:45 +0900

    하나의 상태가 여러 property 를 갖는 경우 연습 하나의 메소드를 활용 완료

--- src/App.jsx
+++ src/App.jsx
@@ -7,44 +7,44 @@ function App(props) {
     price: 0,
     company: "",
   });
-
-  function handleNameChange(e) {
-    setCar({ ...car, name: e.target.value });
-  }
-  function handlePriceChange(e) {
+  function handleChange(e) {
     const { ...nextCar } = car;
-    nextCar.price = e.target.value;
+    nextCar[e.target.name] = e.target.value;
     setCar(nextCar);
-  }
-  function handleCompanyChange(e) {
-    const { ...nextCar } = car;
-    nextCar.company = e.target.value;
-    setCar(nextCar);
-  }
-  function handleModelChange(e) {
-    setCar({ ...car, model: e.target.value });
   }
 
   return (
     <div>
       <div>
-        <input type="text" placeholder={"이름"} onChange={handleNameChange} />
+        <input
+          type="text"
+          placeholder={"이름"}
+          name={"name"}
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <input type="text" placeholder={"모델"} onChange={handleModelChange} />
+        <input
+          type="text"
+          placeholder={"모델"}
+          name={"model"}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <input
           type="text"
           placeholder={"회사"}
-          onChange={handleCompanyChange}
+          name={"company"}
+          onChange={handleChange}
         />
       </div>
       <div>
         <input
           type="number"
           placeholder={"가격"}
-          onChange={handlePriceChange}
+          name={"price"}
+          onChange={handleChange}
         />
       </div>
 
