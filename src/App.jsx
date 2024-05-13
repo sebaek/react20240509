972ede4a7248645507d0833e87fcc47ecfe70797
sebaek <sebaek@gmail.com>
2024-05-13 14:07:12 +0900

    state 연습 시작

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+function MyNumber() {
+  // todo : code 작성
+  return (
+    <div>
+      {/*number의 초기값은 1000*/}
+      <div>{number}</div>
+
+      <div>
+        <button>1씩 감소</button>
+      </div>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyNumber />
+    </div>
+  );
 }
 
 export default App;
