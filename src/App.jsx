cf166c79330759c0ca8710ad05db0d8e57487232
sebaek <sebaek@gmail.com>
2024-05-13 15:32:24 +0900

    input에 입력한값 list에 추가해서 보여주기 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
   // input 입력한 값 list에 추가하기
-  return <div></div>;
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+
+  function handleClick() {
+    if (text.length > 0) {
+      const [...nextList] = list;
+      nextList.push(text);
+      setList(nextList);
+      setText("");
+    }
+  }
+
+  return (
+    <div>
+      <input
+        value={text}
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={handleClick}>추가</button>
+      <ul>
+        {list.map((elem) => (
+          <li>{elem}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
