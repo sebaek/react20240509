105b30ccccb3db02214fbc438cf228cce32b5c12
sebaek <sebaek@gmail.com>
2024-05-13 16:02:42 +0900

    여러 state 사용 예제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
+  const [name, setName] = useState("");
+  const [hobby, setHobby] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+
   return (
     <div>
       <div>
-        <input type="text" />
+        <input type="text" onChange={(e) => setName(e.target.value)} />
       </div>
       <div>
-        <input type="text" />
+        <input type="text" onChange={(e) => setHobby(e.target.value)} />
       </div>
       <div>
-        <input type="text" />
+        <input type="text" onChange={(e) => setAddress(e.target.value)} />
       </div>
       <div>
-        <input type="text" />
+        <input type="text" onChange={(e) => setCity(e.target.value)} />
       </div>
 
       <div>
