fcc6535bb8465a977163ff00e9c5c26beb3697b7
sebaek <sebaek@gmail.com>
2024-05-13 15:43:13 +0900

    여러 state 사용 예제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+
+      <div>
+        {name}, {hobby}, {address}, {city}
+      </div>
+    </div>
+  );
 }
 
 export default App;
