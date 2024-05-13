9aa52834fbc5dab9107548f69ba841c887f4a2cd
sebaek <sebaek@gmail.com>
2024-05-13 11:11:29 +0900

    default export

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+// default export는 아무 이름으로 import할 수 있음, 중괄호 없이
+import val, { v1 } from "./MyValues.jsx";
+import someVal from "./MyComps.jsx";
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>{v1}</div>
+      <div>{val}</div>
+      <div>{someVal}</div>
+    </div>
+  );
 }
 
 export default App;
