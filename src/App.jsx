60e2d823c5875076d7ed641e38fde7a5496c2a92
sebaek <sebaek@gmail.com>
2024-05-13 11:46:53 +0900

    상태가 바뀔 때만 다시 그림

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  const [val, setVal] = useState(0);
+  console.log("다시 그려짐");
+  return (
+    <div>
+      <button onClick={() => setVal(0)}>클릭 {val}</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
