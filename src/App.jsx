7fdd3787693bda80ba4bfc7af764b703595d326a
sebaek <sebaek@gmail.com>
2024-05-13 14:25:00 +0900

    state 변경 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+const nameList = ["손흥민", "박지성", "이강인", "차범근", "황희찬", "김민재"];
+
 function App(props) {
-  return <div></div>;
+  // todo: 코드 작성
+
+  return (
+    <div>
+      <button>이전</button>
+      <button>다음</button>
+      <div>{name}</div>
+    </div>
+  );
 }
 
 export default App;
