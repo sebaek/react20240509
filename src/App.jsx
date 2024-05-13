962bdc80d5ebb36cb4f86ce6d33f37718eaefdef
sebaek <sebaek@gmail.com>
2024-05-13 14:42:43 +0900

    state 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // input에 입력된 값이 p에 출력되도록 코드 작성
+  return (
+    <div>
+      <input type="text" />
+      <p></p>
+    </div>
+  );
 }
 
 export default App;
