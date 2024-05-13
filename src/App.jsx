a5489c455344b692a2510036777a58f1da9c5213
sebaek <sebaek@gmail.com>
2024-05-13 16:18:31 +0900

    하나의 상태가 여러 property 를 갖는 경우 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+
+      <ul>
+        <li>이름 : {car.name}</li>
+        <li>모델 : {car.model}</li>
+        <li>회사 : {car.company}</li>
+        <li>가격 : {car.price}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
