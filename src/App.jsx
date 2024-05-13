7f64bde894798eb80e2e64df156a67ff03f65604
sebaek <sebaek@gmail.com>
2024-05-13 15:02:46 +0900

    state 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
   // input에 입력된 값이 p에 출력되도록 코드 작성
+  const [text, setText] = useState("");
+
   return (
     <div>
-      <input type="text" />
-      <p></p>
+      <input type="text" onChange={(e) => setText(e.target.value)} />
+      <p>{text}</p>
     </div>
   );
 }
