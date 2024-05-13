d8f5d13599075b009cb9b7ffd5848dc6553e4025
sebaek <sebaek@gmail.com>
2024-05-13 15:04:47 +0900

    state 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ function App(props) {
   // input에 입력된 값이 p에 출력되도록 코드 작성
   const [text, setText] = useState("");
 
+  const isTooLong = text.length > 10;
+
   return (
     <div>
       <input type="text" onChange={(e) => setText(e.target.value)} />
       <p>{text}</p>
+      {isTooLong && <p>11자 이상 입력할 수 없습니다.</p>}
     </div>
   );
 }
