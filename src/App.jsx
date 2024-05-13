4f325a97516dfcee54e5b3df79d7a97f3e8d9347
sebaek <sebaek@gmail.com>
2024-05-13 11:03:14 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { Badge, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <Button colorScheme={"blue"}>클릭</Button>
-        <Button colorScheme={"green"}>
-          알림 <Badge>new</Badge>
-        </Button>
-      </div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
