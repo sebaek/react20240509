64ecc71207476d405390729761a145e4cc8f5e95
sebaek <sebaek@gmail.com>
2024-05-13 14:31:42 +0900

    state 변경 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const nameList = ["손흥민", "박지성", "이강인", "차범근", "황희찬", "김민재"];
 
 function App(props) {
-  // todo: 코드 작성
+  const [index, setIndex] = useState(0);
+  const i = (nameList.length + index) % nameList.length;
+  const name = nameList[i];
 
   return (
     <div>
-      <button>이전</button>
-      <button>다음</button>
+      <button onClick={() => setIndex(index - 1)}>이전</button>
+      <button onClick={() => setIndex(index + 1)}>다음</button>
       <div>{name}</div>
     </div>
   );
