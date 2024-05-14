50aab6b1a96180c3067302d9be5733327cc66736
sebaek <sebaek@gmail.com>
2024-05-14 15:45:56 +0900

    axios로 ajax get 요청 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <button onClick={handleClick1}>/path1 get 요청</button>
+      </div>
+      <div>
+        <button onClick={handleClick2}>
+          /path2 get 요청, city, address 요청 파라미터 첨부
+        </button>
+      </div>
+      <div>
+        <button onClick={handleClick3}>
+          /path3 get 요청, name, age 요청 파라미터가 첨부
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
