6b49aca89d27dc60e495f64eadd49c5e4f1a1866
sebaek <sebaek@gmail.com>
2024-05-14 16:09:35 +0900

    axios로 ajax get 요청 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
+  function handleClick1() {
+    axios.get("/path1");
+  }
+
+  function handleClick2() {
+    const params = new URLSearchParams();
+    params.append("city", "서울");
+    params.append("address", "신촌");
+    axios.get(`/path2?${params.toString()}`);
+  }
+
+  function handleClick3() {
+    const params = new URLSearchParams();
+    params.append("name", "흥민");
+    params.append("age", 44);
+
+    axios.get(`/path3?${params.toString()}`);
+  }
+
   return (
     <div>
       <div>
