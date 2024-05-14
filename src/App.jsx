615c352eb2c53cfc2d94351972418ffd1c4d04c1
sebaek <sebaek@gmail.com>
2024-05-14 10:05:04 +0900

    컴포넌트가 트리에서 떨어진 후 다시 붙으면 initial rendering 됨 연습 시작

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <input type="checkbox" />
+      <MyBox />
+      {/*
+     <div>
+      <button>up</button>
+      <p>{count}</p>
+     </div
+
+     */}
+    </div>
+  );
 }
 
 export default App;
