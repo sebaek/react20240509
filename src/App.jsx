b53b72687983b8e10721bd54afed16315d607ded
sebaek <sebaek@gmail.com>
2024-05-14 09:23:00 +0900

    컴포넌트는 각자 자기의 상태만 갖는다. 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyBox />
+      <hr />
+      <MyBox />
+      {/*
+     <div>
+       <input />
+       <p></p>
+     </div>
+     <hr />
+     <div>
+       <input />
+       <p></p>
+     </div>
+
+     */}
+    </div>
+  );
 }
 
 export default App;
