39adf7ba165b2d2f52b087df2a2c961c9abeee00
sebaek <sebaek@gmail.com>
2024-05-14 10:35:27 +0900

    "lift state up"으로 컴포넌트의 상태 유지 가능 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 
-function MyBox() {
-  const [count, setCount] = useState(0);
+function MyBox({ count, onClick }) {
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>UP</button>
+      <button onClick={onClick}>UP</button>
       <p>{count}</p>
     </div>
   );
 }
-
+// todo : MyBox 가 tree 에 떨어진 후 다시 붙어도 count 상태가 유지될 수
+//  있도록 코드 수정
 function App(props) {
   const [display, setDisplay] = useState(true);
+  const [count, setCount] = useState(0);
+
+  function handleUpdateCount() {
+    setCount(count + 1);
+  }
 
   return (
     <div>
@@ -20,7 +25,7 @@ function App(props) {
         checked={display}
         onChange={(e) => setDisplay(e.target.checked)}
       />
-      {display && <MyBox />}
+      {display && <MyBox count={count} onClick={handleUpdateCount} />}
       {/*
      <div>
       <button>up</button>
