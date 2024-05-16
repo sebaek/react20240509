c8fdfed2d05b1597e95b16d99d65f9359ee1793b
sebaek <sebaek@gmail.com>
2024-05-16 14:12:36 +0900

    응답받기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClick1() {
+    axios.get("/api/main42/sub1").then((param) => console.log(param.data));
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>응답 받기</button>
+      <br />
+      {/* 버튼이 클릭되면 /api/main42/sub2 get 요청 후
+      응답본문을 콘솔에 출력 */}
+      <button onClick={handleClick2}>응답 받기</button>
+    </div>
+  );
 }
 
 export default App;
