2e3cd966aab2800ae6ab681f8dc71bee146cc435
sebaek <sebaek@gmail.com>
2024-05-16 10:38:47 +0900

    /api/someurl4의 get/post 요청 버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,24 @@ function App(props) {
     axios.post("/api/someurl3", params);
   }
 
+  function handleClickButton7() {
+    const data = new URLSearchParams();
+    data.append("address", "신촌");
+    data.append("birthDate", "2000-01-01");
+    data.append("inserted", "1999-12-12T13:12:11");
+
+    axios.get(`/api/someurl4?${data}`);
+  }
+
+  function handleClickButton8() {
+    const params = new URLSearchParams();
+    params.append("foods", "pizza");
+    params.append("foods", "coffee");
+    params.append("item", "iphone");
+
+    axios.post("/api/someurl4", params);
+  }
+
   return (
     <div>
       <button onClick={handleClickButton1}>get 요청</button>
@@ -49,8 +67,8 @@ function App(props) {
       <button onClick={handleClickButton5}>get 요청 with query string</button>
       <button onClick={handleClickButton6}>post 요청 with data</button>
       {/* /api/someurl4 get/post with data */}
-      <button>get 요청 with data</button>
-      <button>post 요청 with data</button>
+      <button onClick={handleClickButton7}>get 요청 with data</button>
+      <button onClick={handleClickButton8}>post 요청 with data</button>
     </div>
   );
 }
