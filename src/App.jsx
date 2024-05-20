9e02532c6f543296e8497549f2d653d59af1c29e
sebaek <sebaek@gmail.com>
2024-05-20 10:26:09 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <Button sx={{ color: "orange" }} colorScheme={"blue"}>
-        Lorem.
-      </Button>
+      <div></div>
     </ChakraProvider>
   );
 }
