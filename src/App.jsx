8266e4bcba8d243ea37e45f2fbbdd2673b2b3196
sebaek <sebaek@gmail.com>
2024-05-20 09:26:56 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <Button colorScheme={"gray"}>Gray </Button>
-        <Button colorScheme={"red"}>Lorem.</Button>
-        <Button colorScheme={"orange"}>Dolores?</Button>
-        <Button colorScheme={"yellow"}>Doloribus!</Button>
-        <Button colorScheme={"green"}>Rerum.</Button>
-        <Button colorScheme={"teal"}>Tempore.</Button>
-        <Button colorScheme={"blue"}>Sequi.</Button>
-        <Button colorScheme={"cyan"}>Sint.</Button>
-        <Button colorScheme={"purple"}>Cupiditate?</Button>
-        <Button colorScheme={"pink"}>Nemo!</Button>
-        <hr />
-        <Button colorScheme={"blue"} isLoading={true}>
-          Lorem.
-        </Button>
-        <hr />
-        <Button colorScheme={"blue"} variant={"solid"}>
-          Lorem.
-        </Button>
-        <Button colorScheme={"blue"} variant={"outline"}>
-          Natus.
-        </Button>
-        <Button colorScheme={"blue"} variant={"ghost"}>
-          Voluptatem?
-        </Button>
-        <Button colorScheme={"blue"} variant={"link"}>
-          Accusantium!
-        </Button>
-      </div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
