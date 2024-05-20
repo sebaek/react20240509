7db62e323f34df6fc69090829cd0b6c7d3bcef83
sebaek <sebaek@gmail.com>
2024-05-20 11:32:41 +0900

    토스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ChakraProvider, useToast } from "@chakra-ui/react";
 
 function App(props) {
+  const toast = useToast();
   return (
     <ChakraProvider>
-      <div></div>
+      <Button
+        onClick={() => {
+          toast({
+            title: "토스트 제목",
+            description: "토스트 내용",
+            position: "top",
+            status: "info",
+            isClosable: true,
+            duration: 1000,
+          });
+        }}
+      ></Button>
     </ChakraProvider>
   );
 }
