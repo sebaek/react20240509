a3a4c53b63f0314ae64ca45882a471fe1ddcd000
sebaek <sebaek@gmail.com>
2024-05-20 14:23:22 +0900

    이벤트 버블링 막기 연습
    stopPropagation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Badge, Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <div></div>
+      <Box onClick={handleBoxClick} w={"200px"} bgColor={"gray"} p={10}>
+        <Button onClick={handleButtonClick} colorScheme={"blue"}>
+          클릭
+          <Badge onClick={handleBadgeClick} ml={3}>
+            new
+          </Badge>
+        </Button>
+      </Box>
     </ChakraProvider>
   );
 }
