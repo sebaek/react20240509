1ae9bc639dc0b6b6208f8ebf19ad24c94dc99876
sebaek <sebaek@gmail.com>
2024-05-20 11:18:21 +0900

    모달

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Button,
+  ChakraProvider,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function App(props) {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
     <ChakraProvider>
-      <div></div>
+      <Button onClick={onOpen}>모달 오픈!</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>TITLE</ModalHeader>
+          <ModalBody>Lorem ipsum dolor.</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button colorScheme={"blue"}>저장</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </ChakraProvider>
   );
 }
