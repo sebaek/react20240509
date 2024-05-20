9a07136b611fdeab595fa9462a5369d42c5f07f2
sebaek <sebaek@gmail.com>
2024-05-20 16:23:53 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 function App(props) {
-  const [name, setName] = useState("");
-  const [file, setFile] = useState([]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    axios.postForm("/api/main45/sub2", {
-      name: name,
-      file: file,
-    });
-  }
-
-  const fileNames = [];
-  for (let i = 0; i < file.length; i++) {
-    fileNames.push(<li>{file.item(i).name}</li>);
-  }
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        이름{" "}
-        <input
-          type="text"
-          name="name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br />
-        <input
-          multiple={true}
-          type="file"
-          name="file"
-          onChange={(e) => setFile(e.target.files)}
-        />
-        <br />
-        <input type="submit" />
-      </form>
-      <div>
-        <ul>{fileNames}</ul>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
