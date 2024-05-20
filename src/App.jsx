cc4100e7e5f8a594910062b161044925d60cb8ab
sebaek <sebaek@gmail.com>
2024-05-20 14:19:14 +0900

    이벤트 버블링 막기
    stopPropagation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
+  function handleButtonClick(e) {
+    // event bubbling 멈추기
+    e.stopPropagation();
+
+    console.log("버튼 클릭됨");
+  }
+
+  function handleBoxClick() {
+    console.log("박스 클릭됨");
+  }
+
   return (
     <ChakraProvider>
-      <div></div>
+      <Box onClick={handleBoxClick} w={"100px"} h={"100px"} bgColor={"orange"}>
+        <Button onClick={handleButtonClick}>클릭</Button>
+      </Box>
     </ChakraProvider>
   );
 }
