299e66014ce9c49ebe8e106f7ae2b7df27d1ada3
sebaek <sebaek@gmail.com>
2024-05-20 10:21:40 +0900

    hover pseudo class

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <div></div>
+      <Box
+        bgColor={"orange"}
+        _hover={{
+          bgColor: "yellow",
+        }}
+      >
+        Lorem ipsum dolor.
+      </Box>
     </ChakraProvider>
   );
 }
