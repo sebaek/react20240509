e628740a02050aea1e7ab03df5c16972fdfac5cb
sebaek <sebaek@gmail.com>
2024-05-20 09:27:15 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div></div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
