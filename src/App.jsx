9475fb02ea6fe245de8901473bf5bb104a2f8b72
sebaek <sebaek@gmail.com>
2024-05-20 10:16:44 +0900

    반응형 스타일

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <div></div>
+      <Box bgColor={"tomato"} w={"500px"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box bgColor={"yellow.700"} w={{ base: "100%", md: "500px" }}>
+        Lorem ipsum dolor.
+      </Box>
     </ChakraProvider>
   );
 }
