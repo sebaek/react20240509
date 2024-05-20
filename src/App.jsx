f9b76e558e84e47cf174829da47d975cccdf865a
sebaek <sebaek@gmail.com>
2024-05-20 14:35:51 +0900

    기본 동작 막기 연습
    preventDefault

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // a 클릭 시
+  // form 서브밋 시
+
+  // 기본 동작 막고,, 콘솔에 간단한 메세지 출력
+  return (
+    <div>
+      <a href="https://www.google.com">구글</a>
+      <hr />
+      <form action="https://www.google.com">
+        <input type="text" />
+        <button>전송</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
