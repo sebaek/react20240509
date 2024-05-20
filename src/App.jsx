5d0a66e667fe36beeb4d6bc5de358a0b2b753b1e
sebaek <sebaek@gmail.com>
2024-05-20 15:01:54 +0900

    기본 동작 막기 연습 완료
    preventDefault

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,24 @@ function App(props) {
   // a 클릭 시
   // form 서브밋 시
 
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("다른 일...");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("서브밋 대신 다른 일...");
+  }
+
   // 기본 동작 막고,, 콘솔에 간단한 메세지 출력
   return (
     <div>
-      <a href="https://www.google.com">구글</a>
+      <a onClick={handleClick} href="https://www.google.com">
+        구글
+      </a>
       <hr />
-      <form action="https://www.google.com">
+      <form onSubmit={handleSubmit} action="https://www.google.com">
         <input type="text" />
         <button>전송</button>
       </form>
